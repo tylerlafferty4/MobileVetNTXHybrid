15b797c3022aeac4584e0ce535816cd52ed492a0
Lafferty, Tyler <TLafferty@national.aaa.com>
2017-11-06 16:22:08 -0500

    Form Validation

--- src/pages/appointments/appointments.ts
+++ src/pages/appointments/appointments.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import {Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { NavController, ToastController } from 'ionic-angular';
+import {Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'page-appointments',
@@ -9,16 +9,18 @@ import {Validators, FormBuilder, FormGroup } from '@angular/forms';
 export class AppointmentPage {
 
   private appointment : FormGroup;
+  userInfo: {name: string, phone: string, address: string, address2: string, city: string, zip: string, email: string, date: string, petName: string, nature: string} =
+            {name: '', phone: '', address: '', address2: '', city: '', zip: '', email: '', date: '', petName: '', nature: ''};
   background = '#50095f';
 
-  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder, public toastCtrl: ToastController) {
     this.appointment = this.formBuilder.group({
       name: [''],
-      phone: ['', Validators.required],
+      phone: ['', Validators.required, this.phoneValidator.bind(this)],
       address: ['', Validators.required],
       address2: [''],
       city: ['', Validators.required],
-      zip: ['', Validators.required, Validators.maxLength(5)],
+      zip: ['', Validators.required],
       email: ['', Validators.required],
       date: ['', Validators.required],
       petName: [''],
@@ -26,8 +28,18 @@ export class AppointmentPage {
     });
   }
 
-  logForm(){
-
+  phoneValidator(control: FormControl): {[s: string]: boolean} {
+    if (control.value !== '') {
+      if (!control.value.match('\\(?\\d{3}\\)?-? *\\d{3}-? *-?\\d{4}')) {
+        return {invalidPhone: true};
+      }
+    }
   }
 
+  logForm() {
+    var toast = this.toastCtrl.create({
+      message: this.userInfo.address
+    });
+    toast.present();
+  }
 }
